cf6016c98ca8fc9f0a50d66ff53ea40cd3c98464
Kas1680 <kas1914080@gmail.com>
2022-04-28 19:49:38 -0700

    April 28

---
+++ CF339.cpp
@@ -0,0 +1,15 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+int main(){
+    string input;
+    cin >> input;
+    
+    int k = input.length();
+    int a[k];
+    for(int i = 0; i < input.length(); i++){
+        if(input[i] != '+'){
+
+        }
+    }
+}
